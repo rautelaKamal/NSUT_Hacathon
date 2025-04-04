4a2d28ac83c92b3504e0a701fd8061a0a6f1a3f3
Your Name <your.email@example.com>
2025-04-04 16:45:24 +0530

    Updates

---
+++ precache-manifest.60271ae756a6489e89578ec4bbe8ee2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6de29a870eed4ac4a9957b3888b5f848",
+    "url": "/kavyapath/index.html"
+  },
+  {
+    "revision": "bb4db412d44bca141d0d",
+    "url": "/kavyapath/static/css/main.2288503e.chunk.css"
+  },
+  {
+    "revision": "09c5da9a6952e92cb521",
+    "url": "/kavyapath/static/js/2.58f150ab.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/kavyapath/static/js/2.58f150ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8efb540fc77fb0fdc66",
+    "url": "/kavyapath/static/js/3.478ac182.chunk.js"
+  },
+  {
+    "revision": "bb4db412d44bca141d0d",
+    "url": "/kavyapath/static/js/main.b16d4932.chunk.js"
+  },
+  {
+    "revision": "1ebadb69ec9fa98a5372",
+    "url": "/kavyapath/static/js/runtime-main.614113ed.js"
+  }
+]);
